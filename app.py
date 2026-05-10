70e436f49a5bf75e1f067e221724df915f273e76
Guilherme Graces <guilhermegracesoli@gmail.com>
2026-05-09 20:36:48 -0400

    Criando a pagina Sobre nós

--- app.py
+++ app.py
@@ -13,13 +13,12 @@ def carregar_modelo():
 tokenizer, model = carregar_modelo()
 
 #   página inicial
-
-def inicial():
-    st.title("fodase")
-    return
+#a criar
 
 
 #   criando uma sidebar
+if "pagina" not in st.session_state:
+    st.session_state.pagina = "chat"
 
 with st.sidebar:
 
@@ -27,23 +26,29 @@ with st.sidebar:
 
     st.divider()
 
-    pagSobre= st.button("🏢 Sobre Nós", use_container_width=True)
+    if st.button("🏢 Sobre Nós", use_container_width=True):
+        st.session_state.pagina = "sobre"
     
 
     st.write("")
 
-    pagChat= st.button("👾 Chat Bot", use_container_width=True)
+    if st.button("👾 Chat Bot", use_container_width=True):
+        st.session_state.pagina = "chat"
     
 
 #   pagina Sobre nós
 
-if pagSobre:
-    st.title("oioi")
+if st.session_state.pagina =="sobre":
+    st.title("Sobre nós")
+    st.divider()
+    st.image("imgs/imagemSobreNos.jpg")
+    st.divider()
+    st.markdown("Somos uma equipe apaixonada por tecnologia, inovação e pelo impacto que a inteligência artificial pode causar na vida das pessoas. Formado por Gabriel Andrade, Guilherme Graces, Antonella Scanoni e Adelano Mascarenhas, nosso grupo desenvolveu um projeto com o objetivo de aproximar a tecnologia das pessoas de forma prática, inteligente e acessível. \n Nosso foco é a criação de um agente de inteligência artificial capaz de conversar de maneira eficiente, natural e dinâmica, oferecendo respostas rápidas, úteis e humanizadas. Mais do que apenas responder perguntas, buscamos construir uma experiência interativa que facilite a comunicação e torne o uso da tecnologia algo simples e agradável para todos. \n Acreditamos que a IA tem o poder de transformar a forma como as pessoas aprendem, trabalham e se conectam. Por isso, unimos criatividade, dedicação e conhecimento para desenvolver uma solução moderna, funcional e preparada para evoluir constantemente junto às necessidades dos usuários. \n Este projeto representa não apenas nosso interesse pela área da tecnologia, mas também nosso compromisso em criar algo inovador, útil e capaz de gerar impacto positivo no cotidiano das pessoas.")
 
 
 # pagina do chatbot
 
-elif pagChat:
+elif st.session_state.pagina =="chat":
 
     st.title("Chatbot")
 
